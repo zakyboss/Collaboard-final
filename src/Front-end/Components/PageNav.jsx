be554254dddb32b70e471cd0637a102c0e56a2f1
Zakyboss <Boochimo8@gmail.com>
2025-03-09 15:55:59 +0300

    Progress

--- src/Front-end/Components/PageNav.jsx
+++ src/Front-end/Components/PageNav.jsx
@@ -3,19 +3,19 @@ import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
 import Logo from "./Logo";
 import UserSideBar from "./UserSideBar";
-
+import useAuthStore from "../Zustand-api/Authentication";
 export default function PageNav() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  
+  const { isAuthenticated } = useAuthStore();
   const toggleSidebar = (e) => {
     e.preventDefault(); // Prevent navigation if the link goes to profile
     setSidebarOpen(!sidebarOpen);
   };
-  
+
   const closeSidebar = () => {
     setSidebarOpen(false);
   };
-  
+
   return (
     <>
       <nav className={styles.nav}>
@@ -27,17 +27,28 @@ export default function PageNav() {
           <li>
             <NavLink to="/contacts">Contacts</NavLink>
           </li>
+          {isAuthenticated ? (
+            <li>
+ <NavLink to="/your-projects"> My Projects </NavLink>
+            </li>           
+          ) : (
+            <li>
+              <NavLink to="/login">Login</NavLink>
+            </li>
+          )}
+
           <li>
-            <NavLink to="/login">Login</NavLink>
-          </li>
-          <li>
-            <NavLink to="/profile" className={styles.profileIcon} onClick={toggleSidebar}>
+            <NavLink
+              to="/profile"
+              className={styles.profileIcon}
+              onClick={toggleSidebar}
+            >
               <i className="fas fa-user-circle"></i>
             </NavLink>
           </li>
         </ul>
       </nav>
-      
+
       <UserSideBar isOpen={sidebarOpen} onClose={closeSidebar} />
     </>
   );
