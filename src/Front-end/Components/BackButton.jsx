52901443ab92ad4aa25b19de6df9e79fed17649d
Zakyboss <Boochimo8@gmail.com>
2025-03-23 10:57:46 +0300

    Reusable back button

--- src/Front-end/Components/BackButton.jsx
+++ src/Front-end/Components/BackButton.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { useNavigate } from 'react-router'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './BackButton.css'; // Import the CSS file
 
-export default function BackButton() {
-    const navigate = useNavigate();
-    function handleBack(){
-   navigate('-1')
-    }
+export default function BackButton({ className = '', text = 'Back', theme = 'primary' }) {
+  const navigate = useNavigate();
+  
+  function handleBack() {
+    navigate(-1);
+  }
+  
   return (
-    <button onClick={handleBack}>Back</button>
-  )
+    <button 
+      onClick={handleBack} 
+      className={`back-button ${theme} ${className}`}
+    >
+      <span className="back-icon">&#8592;</span>
+      <span className="back-text">{text}</span>
+    </button>
+  );
 }
