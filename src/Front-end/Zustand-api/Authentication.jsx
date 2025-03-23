ca01a93a0d91f3a021f1ecac41a8dac6bcfb93e8
Zakyboss <Boochimo8@gmail.com>
2025-03-23 13:07:56 +0300

    Progress

--- src/Front-end/Zustand-api/Authentication.jsx
+++ src/Front-end/Zustand-api/Authentication.jsx
@@ -5,8 +5,31 @@ const useAuthStore = create((set) => ({
   user: null,
   isAuthenticated: false,
 
-  login: (userData) => set({ user: userData, isAuthenticated: true }),
-  logout: () => set({ user: null, isAuthenticated: false }),
+  // Called after successful login
+  login: (userData) => set({
+    user: userData,
+    isAuthenticated: true,
+  }),
+
+  // Called when user logs out
+  logout: () => set({
+    user: null,
+    isAuthenticated: false,
+  }),
+
+  // Called when user info is updated (e.g., after editing profile)
+  updateUser: (newUserData) => set((state) => {
+    // Merge the new fields into the existing user object
+    const updatedUser = {
+      ...state.user,
+      ...newUserData,
+    };
+
+    return {
+      user: updatedUser,
+      isAuthenticated: true, // remains true if user was already authenticated
+    };
+  }),
 }));
 
 export default useAuthStore;
